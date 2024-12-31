7b9a2e463d0e79d5272f94de24963326f0a70c2c
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-12-31 14:50:24 -0500

    Image Kit
    working on keys and adding images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] relative items-center justify-items-center h-96 p-8 pb-20 gap-16 sm:p-20 font-display">
      
      <div className="flex relative justify-center">
-    <Image src="https://ik.imagekit.io/yllpuzoch/meme%20library%20/cloned.jpg?updatedAt=1733602656646" alt="clone meme"></Image>
+    
      </div>
 
 

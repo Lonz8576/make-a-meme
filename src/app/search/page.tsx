af40df6b6e624cf71a796bf1bbf024652682d6b0
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2025-01-21 13:27:22 -0500

    Creating search page
    get imageKit list of mems from search query

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,24 @@
+import ImageKit from "imagekit";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const ImageKit = require('imagekit');
 
-export default function SearchPage() {
-    return (
+const imagekit = new ImageKit({
+    publicKey:process.env.NEXT_PUBLIC_PUBLIC_KEY,
+    privateKey:process.env.PRIVATE_KEY,
+    urlEndpoint:process.env.NEXT_PUBLIC_URL_ENDPOINT,
+});
+
+export default async function SearchPage( {
+    searchParams,
+}: {
+    searchParams: { q: string };
+}) {
+    const files = await imagekit.listFiles({
+        searchQuery: `name:${searchParams.q}`,
+    });
+
+    return 
     <div>SearchPage</div>
-    )
+    
 }

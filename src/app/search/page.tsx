87a06e8660314f0f8f0a6ea00301006a964ec0b8
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2025-01-24 16:14:29 -0500

    Define params
    for files searched

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const ImageKit = require('imagekit');
 
@@ -24,7 +27,7 @@ export default async function SearchPage( {
 
     return (
     <div>
-        {files.map((file) => {
+        {files.map((file: { fieldId: Key | null | undefined; name: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => {
             return <div key={file.fieldId}>{file.name}</div>;
 
         })}

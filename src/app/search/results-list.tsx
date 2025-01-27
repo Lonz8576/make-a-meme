ecc3b0646e6c1654577a647d5a2a6f24ad04fe7d
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2025-01-27 14:52:39 -0500

    Results-list created

--- src/app/search/results-list.tsx
+++ src/app/search/results-list.tsx
@@ -4,40 +4,34 @@ import { FileObject } from "imagekit/dist/libs/interfaces";
 import { IKImage } from "imagekitio-next";
 
 
-export default function ResultsList({ files }: { files: FileObject[] }) {
+export function ResultsList({ 
+    files
+
+     }: {
+     files: FileObject[];
+     }) {
 
     return (
         <div>
-        {files.map({file} => {
-            return (
-            
+            <div>
+        {files.map((file) => (
+  
         <IKImage
             key={file.fileId}
             src={file.filePath}
             alt={file.name}
+            urlEndpoint={process.env.NEXT_PUBLIC_URL_ENDPOINT}
             width={300}
             height={300}
             />
-        );
-    })}
+        ))};
         
+    
         </div>
-    )
+        </div>
+    );
 }
+
 /*
-{files.map({file} => {
-    return (
-    <IKImage 
-    key={file.fileId}
-    src={file.filePath}
-    alt={file.name}
-    width={300}
-    height={300}
-    />
-
-
-);
-})}
-
 import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 */
